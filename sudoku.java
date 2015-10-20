4e74e8661b3314dfaa1cbce010bf384616715830
Ankita <ankita.joshi1591@gmail.com>
2015-10-20 02:10:27 -0400

    3x3 block neighbours

--- sudoku.java
+++ sudoku.java
@@ -66,10 +66,49 @@ public class Sudoku{
 			for(int j=i+9;j<81;j=j+9){
 				
 				Arcs arc = new Arcs(i,j);	
-				queue.add(arc);
+
 			}
 		
 		//add all 3X3 neighbours in the queue
+		
+		int[] set0 ={0,1,2,9,10,11,18,19,20};
+		int[] set1 ={3,4,5,12,13,14,21,22,23};		
+		int[] set2 ={6,7,8,15,16,17,24,25,26};
+		int[] set3 ={27,28,29,36,37,38,45,46,47};
+		int[] set4 ={30,31,32,39,40,41,48,49,50};
+		int[] set5 ={33,34,35,42,43,44,51,52,53};
+		int[] set6 ={54,55,56,63,64,65,72,73,74};
+		int[] set7 ={57,58,59,66,67,68,75,76,77};
+		int[] set8 ={60,61,62,69,70,71,78,79,80};
+		
+		for(int i=0;i<9;i++)
+		{
+			for(int j=i+1;j<9;j++)
+			{
+				Arcs arc = new Arcs(set0[i],set0[j]);
+				queue.add(arc);
+				Arcs arc2 = new Arcs(set1[i],set1[j]);
+				queue.add(arc2);
+				Arcs arc3 = new Arcs(set2[i],set2[j]);
+				queue.add(arc3);
+				Arcs arc4 = new Arcs(set3[i],set3[j]);
+				queue.add(arc4);
+				Arcs arc5 = new Arcs(set4[i],set4[j]);
+				queue.add(arc5);
+				Arcs arc6 = new Arcs(set5[i],set5[j]);
+				queue.add(arc6);
+				Arcs arc7 = new Arcs(set6[i],set6[j]);
+				queue.add(arc7);
+				Arcs arc8 = new Arcs(set7[i],set7[j]);
+				queue.add(arc8);
+				Arcs arc9 = new Arcs(set8[i],set8[j]);
+				queue.add(arc9);
+				
+			}
+		}
+		
+		
+		
 		
 	}
 
